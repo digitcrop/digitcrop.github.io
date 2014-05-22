7cc70d22f007adea8a70d28444cc7e4e4e4740e8
Noufal Binu <digitcrop@gmail.com>
2014-05-22 09:31:08 +0530

    Update smileys.js

--- smileys.js
+++ smileys.js
@@ -7,7 +7,7 @@ if (a) {
 	b = a.getElementsByTagName("p");
 	for (i = 0; i < b.length; i++) {
 		if (b.item(i).getAttribute('CLASS') == 'comment-content') { 
-_str = b.item(i).innerHTML.replace(/:\)\)/gi, "<img src='http://1.bp.blogspot.com/-UqwtQ2vN2Kc/U31WSJiAdyI/AAAAAAAABss/V3BRedy4Mr0/s1600/smile3.gif' alt='' class='smiley'/>"); 
+_str = _str.replace(/:\)/gi, "<img src='http://1.bp.blogspot.com/-UqwtQ2vN2Kc/U31WSJiAdyI/AAAAAAAABss/V3BRedy4Mr0/s1600/smile3.gif' alt='' class='smiley'/>"); 
 b.item(i).innerHTML = _str; 
 } 
 } 

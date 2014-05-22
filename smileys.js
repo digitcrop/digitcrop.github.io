6693b55a8e366a286afecf4dad67790a1081547b
Noufal Binu <digitcrop@gmail.com>
2014-05-22 09:35:58 +0530

    Update smileys.js

--- smileys.js
+++ smileys.js
@@ -8,7 +8,7 @@ if (a) {
 	for (i = 0; i < b.length; i++) {
 		if (b.item(i).getAttribute('CLASS') == 'comment-content') { 
 _str = b.item(i).innerHTML.replace(/:\)\)/gi, "<img src='http://2.bp.blogspot.com/-Ph2wxEvy2mc/UTIr8-fdHjI/AAAAAAAABAo/3use2PsPhMA/s1600/taunt.gif' alt='' class='smiley'/>");
-_str = _str.replace(/:\)/gi, "<img src='http://1.bp.blogspot.com/-UqwtQ2vN2Kc/U31WSJiAdyI/AAAAAAAABss/V3BRedy4Mr0/s1600/smile3.gif' alt='' class='smiley'/>"); 
+_str = _str.replace(/:\)/gi, "<img src='http://4.bp.blogspot.com/-k4ujC23cSgI/UTIq7eUkmuI/AAAAAAAABAQ/YUNL9_RhDB0/s1600/smile3.gif' alt='' class='smiley'/>"); 
 b.item(i).innerHTML = _str; 
 } 
 } 
